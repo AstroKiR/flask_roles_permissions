1652a9631324c160d70cb742ec6ac0d66114fca4
kir <k.prokopenkov@mobiles.by>
2020-04-19 04:25:34 +0300

    base template, view form login

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,16 @@
+from flask import render_template
+
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'index'
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    login_form = LoginForm()
+    return render_template('login.html', form=login_form)
